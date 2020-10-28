736e1e74d74cf7def76fbcc1eb3f2b2d8d300ad1
Arei <sergzven@gmail.com>
2020-10-28 12:34:44 +0200

    added bootstrat starter template

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* пути к исходным файлам (src), к готовым файлам (build), а также к тем, за изменениями которых нужно наблюдать (watch) */
+/* this variable contains paths to source files (src), to built files (build) and to files that should be watched (watch) */
 var path = {
     build: {
         html:   'build/',
@@ -34,7 +34,7 @@ var path = {
     clean: './build/*'
 };
 
-/* настройки сервера */
+/* local server config */
 var config = {
     server: {
         baseDir: './build'
@@ -47,7 +47,7 @@ var sortVendors = function (arr) {
     return arr.map(function (item) {return item.slice(item.lastIndexOf('/') + 1)})
 }
 
-/* подключаем gulp и плагины */
+/* importing gulp plugins */
 var gulp = require('gulp'),  // подключаем Gulp
     webserver = require('browser-sync'), // сервер для работы и автоматического обновления страниц
     order = require('gulp-order'), // модуль для сортировки файлов
@@ -68,100 +68,100 @@ var gulp = require('gulp'),  // подключаем Gulp
     rimraf = require('gulp-rimraf'), // плагин для удаления файлов и каталогов
     rename = require('gulp-rename');
 
-/* задачи */
+/* Tasks */
 
-// запуск сервера
+// Launch local server
 gulp.task('webserver', function () {
     webserver(config);
 });
 
-gulp.task('vendor', function () {
-    return gulp.src(path.src.vendor)
-        .pipe(gulp.dest((path.build.vendor)))
-        .pipe(order(sortVendors(path.src.vendor))) //sort vendor files as they are ordered in src list
-        .pipe(concat('vendors.js'))
-        .pipe(gulp.dest(path.build.vendor));
-});
-
-// сбор html
-gulp.task('html:build', function () {
-    return gulp.src(path.src.html) // выбор всех html файлов по указанному пути
-        .pipe(plumber()) // отслеживание ошибок
-        .pipe(rigger()) // импорт вложений
-        .pipe(gulp.dest(path.build.html)) // выкладывание готовых файлов
-        .pipe(webserver.reload({ stream: true })); // перезагрузка сервера
-});
-
-
-// сбор XML, JSON конфига
+// prepare XML, JSON configs
 gulp.task('config:build', function () {
-    return gulp.src(path.src.config) // выбор всех html файлов по указанному пути
-        .pipe(gulp.dest(path.build.html)) // выкладывание готовых файлов
-        .pipe(webserver.reload({ stream: true })); // перезагрузка сервера
+    return gulp.src(path.src.config) // take all required files
+        .pipe(gulp.dest(path.build.html)) // add files to the compiled project
+        .pipe(webserver.reload({ stream: true })); // reload local server
 });
 
-// сбор стилей
+// Build all vendor files and combine into one 'vendors.js' file
+gulp.task('vendor', function () {
+    return gulp.src(path.src.vendor) // get all vendors files
+        .pipe(gulp.dest((path.build.vendor))) // add vendors files to the compiled project
+        .pipe(order(sortVendors(path.src.vendor))) // sort vendor files as they are ordered in src list
+        .pipe(concat('vendors.js')) // concatenate all vendor files into one file
+        .pipe(gulp.dest(path.build.vendor)); // add to the compiled project
+});
+
+// build html
+gulp.task('html:build', function () {
+    return gulp.src(path.src.html) // get all HTML files from source folder
+        .pipe(plumber()) // check for errors
+        .pipe(rigger()) // include all attachments inside HTML files
+        .pipe(gulp.dest(path.build.html)) // add files to the compiled project
+        .pipe(webserver.reload({ stream: true })); // reload local server
+});
+
+// build styles
 gulp.task('css:build', function () {
-    return gulp.src(path.src.style) // получим main.scss
-        .pipe(plumber()) // для отслеживания ошибок
-        .pipe(sourcemaps.init()) // инициализируем sourcemap
+    return gulp.src(path.src.style) // get main.scss
+        .pipe(plumber()) // check for errors
+        .pipe(sourcemaps.init()) // initialise sourcemap
         .pipe(sass()) // scss -> css
-        .pipe(autoprefixer()) // добавим префиксы
-        .pipe(gulp.dest(path.build.css))
-        .pipe(webserver.reload({ stream: true })); // перезагрузим сервер
+        .pipe(autoprefixer()) // add old browsers prefixes
+        .pipe(gulp.dest(path.build.css)) // add files to the compiled project
+        .pipe(webserver.reload({ stream: true })); // reload local server
 });
 
-// сбор стилей
+// build minified styles
 gulp.task('css:build:min', function () {
-    return gulp.src(path.src.style) // получим main.scss
-        .pipe(plumber()) // для отслеживания ошибок
-        .pipe(sourcemaps.init()) // инициализируем sourcemap
+    return gulp.src(path.src.style) // get main.scss
+        .pipe(plumber()) // check for errors
+        .pipe(sourcemaps.init()) // initialise sourcemap
         .pipe(sass()) // scss -> css
-        .pipe(autoprefixer()) // добавим префиксы
-        .pipe(gulp.dest(path.build.css))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(cleanCSS()) // минимизируем CSS
-        .pipe(sourcemaps.write('./')) // записываем sourcemap
-        .pipe(gulp.dest(path.build.css)) // выгружаем в build
-        .pipe(webserver.reload({ stream: true })); // перезагрузим сервер
+        .pipe(autoprefixer()) // add old browsers prefixes
+        .pipe(gulp.dest(path.build.css)) // add files to the compiled project
+        .pipe(rename({ suffix: '.min' })) // add .min suffix for minified version
+        .pipe(cleanCSS()) // make minified version of CSS
+        .pipe(sourcemaps.write('./')) // wright sourcemap
+        .pipe(gulp.dest(path.build.css)) // add files to the compiled project
+        .pipe(webserver.reload({ stream: true })); // reload local server
 });
 
-// сбор js
+// build js
 gulp.task('js:build', function () {
-    return gulp.src(path.src.js) // получим файл main.js
+    return gulp.src(path.src.js) // get main.js
         .pipe(babel({presets: ['@babel/env']})) //convert to es5 with babel
-        .pipe(plumber()) // для отслеживания ошибок
-        .pipe(rigger()) // импортируем все указанные файлы в main.js
-        .pipe(gulp.dest(path.build.js))
-        .pipe(webserver.reload({ stream: true })); // перезагрузим сервер
+        .pipe(plumber()) // check for errors
+        .pipe(rigger()) // include all attachments inside JS file
+        .pipe(gulp.dest(path.build.js)) // add files to the compiled project
+        .pipe(webserver.reload({ stream: true })); // reload local server
 });
 
-// сбор js
+// build minified js
 gulp.task('js:build:min', function () {
-    return gulp.src(path.src.js) // получим файл main.js
+    return gulp.src(path.src.js) // get main.js
         .pipe(babel({presets: ['@babel/env']})) //convert to es5 with babel
-        .pipe(plumber()) // для отслеживания ошибок
-        .pipe(rigger()) // импортируем все указанные файлы в main.js
-        .pipe(gulp.dest(path.build.js))
+        .pipe(plumber()) // check for errors
+        .pipe(rigger()) // include all attachments inside JS file
+        .pipe(gulp.dest(path.build.js)) // add files to the compiled project
         .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.init()) //инициализируем sourcemap
-        .pipe(uglify()) // минимизируем js
-        .pipe(sourcemaps.write('./')) //  записываем sourcemap
-        .pipe(gulp.dest(path.build.js)) // положим готовый файл
-        .pipe(webserver.reload({ stream: true })); // перезагрузим сервер
+        .pipe(sourcemaps.init()) //initialise sourcemap
+        .pipe(uglify()) // do minification
+        .pipe(sourcemaps.write('./')) // wright sourcemap
+        .pipe(gulp.dest(path.build.js)) // add files to the compiled project
+        .pipe(webserver.reload({ stream: true })); // reload local server
 });
 
-// перенос шрифтов
+// copy fonts
 gulp.task('fonts:build', function () {
-    return gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts));
+    return gulp.src(path.src.fonts) // get fonts from source
+        .pipe(gulp.dest(path.build.fonts)); // add fonts to the compiled project
 });
 
-// обработка картинок
+// build images
 gulp.task('image:build', function () {
-    return gulp.src(path.src.img) // путь с исходниками картинок
+    return gulp.src(path.src.img) // get images from source
         .pipe(cache(
-            imagemin([ // сжатие изображений
+            imagemin([ // compress images
                 imagemin.gifsicle({ interlaced: true }),
                 imageminMozjpeg({
                     progressive: true,
@@ -170,21 +170,21 @@ gulp.task('image:build', function () {
                 imageminOptipng(),
                 imageminSvgo({ plugins: [{removeViewBox: false}] })
             ])
-        )).pipe(gulp.dest(path.build.img)); // выгрузка готовых файлов
+        )).pipe(gulp.dest(path.build.img)); // add files to the compiled project
 });
 
-// удаление каталога build
+// removing build folder
 gulp.task('clean:build', function () {
     return gulp.src(path.clean, { read: false })
         .pipe(rimraf());
 });
 
-// очистка кэша
+// clear caches
 gulp.task('cache:clear', function () {
     cache.clearAll();
 });
 
-// сборка
+// Build task
 gulp.task('build',
     gulp.series('clean:build',
         gulp.parallel(
@@ -199,7 +199,7 @@ gulp.task('build',
     )
 );
 
-// сборка минифицированой версии
+// Build minified version
 gulp.task('build:min',
     gulp.series('clean:build',
         gulp.parallel(
@@ -214,7 +214,7 @@ gulp.task('build:min',
     )
 );
 
-// запуск задач при изменении файлов
+// run tasks on files change
 gulp.task('watch', function () {
     gulp.watch(path.watch.html, gulp.series('html:build'));
     gulp.watch(path.watch.css, gulp.series('css:build'));
@@ -224,7 +224,7 @@ gulp.task('watch', function () {
     gulp.watch(path.watch.config, gulp.series('config:build'));
 });
 
-// задача по умолчанию
+// Default task
 gulp.task('default', gulp.series(
     'build',
     gulp.parallel('webserver','watch')
